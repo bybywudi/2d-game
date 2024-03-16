7bd30a2a86bf4e54798fadd4d8b993070c4a4892
baiyang <baiyangwd@163.com>
2024-03-16 23:14:19 +0800

    水下场景

--- worlds/world.gd
+++ worlds/world.gd
@@ -6,16 +6,22 @@ extends Node2D
 @onready var tile_map: TileMap = $TileMap
 @onready var camera_2d: Camera2D = $Player/Camera2D
 @onready var player: CharacterBody2D = $Player
+@onready var trees: ParallaxLayer = $Decorations/Background/Trees
+@onready var flower_animations: AnimatedSprite2D = $Decorations/BackgroundDecorations/Trees/FlowerAnimations
 
 
 func _ready() -> void:
 	var used := tile_map.get_used_rect().grow(-1)
 	var tile_size := tile_map.tile_set.tile_size
-	
-	camera_2d.limit_top = used.position.y * tile_size.y
-	camera_2d.limit_right = used.end.x * tile_size.x
-	camera_2d.limit_bottom = used.end.y * tile_size.y
-	camera_2d.limit_left = used.position.x * tile_size.x
+	var children := flower_animations.get_children(false)
+	flower_animations.play("default", 1.0, false)
+	for child in children:
+		child.play("default", 1.0, false)
+	#trees.modulate = Color(0,0,1,1)
+	#camera_2d.limit_top = used.position.y * tile_size.y
+	#camera_2d.limit_right = used.end.x * tile_size.x
+	#camera_2d.limit_bottom = used.end.y * tile_size.y
+	#camera_2d.limit_left = used.position.x * tile_size.x
 	camera_2d.reset_smoothing()
 	
 	if bgm:
